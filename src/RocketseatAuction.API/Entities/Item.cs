94fcdac730d3fcea981ced15da1389206daccb7a
Gabriel Manika Koeb <gmkoeb@gmail.com>
2024-02-10 14:25:53 -0300

    Feat/Adiciona enum na entity Item
    - Foi criada a pasta Enums com o arquivo que define enums para Conditions de Items

--- src/RocketseatAuction.API/Entities/Item.cs
+++ src/RocketseatAuction.API/Entities/Item.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
+﻿using RocketseatAuction.API.Enums
+using System.ComponentModel.DataAnnotations.Schema;
 namespace RocketseatAuction.API.Entities;
 
 [Table("Items")]
@@ -9,7 +9,7 @@ public class Item
     public string Name { get; set; } = string.Empty;
     public string Brand { get; set; } = string.Empty;
 
-    public int Condition { get; set; }
+    public Condition Condition { get; set; }
     public decimal BasePrice { get; set; }
 
     public int AuctionId { get; set; }
