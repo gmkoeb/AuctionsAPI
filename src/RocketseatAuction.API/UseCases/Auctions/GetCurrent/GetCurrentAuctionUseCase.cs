3eba3395ec76faf5a0946fe4fcc770b1696cd44c
Gabriel Manika Koeb <gmkoeb@gmail.com>
2024-02-11 10:57:09 -0300

    Refactor/Acessa db via repositories
    - Refatora código da classe AuctionDbContext para acessar o banco de dados via repositories ao invés de acessar diretamente.
    
    - Refatora código dos useCases para acessar dados via interfaces ao invés de inicializar banco de dados diretamente

--- src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -1,16 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using RocketseatAuction.API.Contracts;
 using RocketseatAuction.API.Entities;
-using RocketseatAuction.API.Repositories;
 
 namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
 
 public class GetCurrentAuctionUseCase
 {
+    private readonly IAuctionRepository _repository;
+    public GetCurrentAuctionUseCase(IAuctionRepository repository) => _repository = repository;
+
     public Auction? Execute()
     {
-        var repository = new RocketseatAuctionDbContext();
-        {
-            return repository.Auctions.Include(auction => auction.Items).FirstOrDefault();
-        };
+        return _repository.GetCurrent(); 
     }
 }
